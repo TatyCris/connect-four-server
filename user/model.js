4da3c91be8cdd43350a418512f039f48a6896783
TATY <tatyccsilva@gmail.com>
2019-07-08 13:53:06 +0200

    Create an user

---
+++ user/model.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize')
+const sequelize = require('../db')
+
+const User = sequelize.define('user', {
+    userName: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+}, {
+        timestamps: false,
+        tableName: 'users'
+    })
+
+module.exports = User
