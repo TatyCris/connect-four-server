c9db1954157b5e05fa31f9b584526db6c4421fc1
“YvanSchaaik” <yvanschaaik@gmail.com>
2019-07-09 16:38:06 +0200

    Fix a bug

--- room/router.js
+++ room/router.js
@@ -4,19 +4,19 @@ const Room = require('./model')
 const auth = require('../auth/middleware')
 const Sse = require('json-sse')
 
-router.get('/rooms', function (req, res, next) {
-    const limit = req.query.limit || 10
-    const offset = req.query.offset || 0
+// router.get('/rooms', function (req, res, next) {
+//     const limit = req.query.limit || 10
+//     const offset = req.query.offset || 0
 
-    Promise.all([
-        Room.count(),
-        Room.findAll({ limit, offset })
-    ])
-        .then((total, rooms) => {
-            res.send({ rooms, total })
-        })
-        .catch(error => next(error))
-})
+//     Promise.all([
+//         Room.count(),
+//         Room.findAll({ limit, offset })
+//     ])
+//         .then((total, rooms) => {
+//             res.send({ rooms, total })
+//         })
+//         .catch(error => next(error))
+// })
 
 Room
     .findAll()
@@ -26,6 +26,13 @@ Room
 
         router.get('/stream', function (req,res,next){
             return stream.init(req, res)
+        })
+
+        router.get('/rooms', function(req, res, next){
+            Room
+                .findAll()
+                .then(room => res.status(200).send(room))
+                .catch(err => next(err))
         })
 
         router.post('/rooms', function (req, res, next) {
@@ -51,31 +58,6 @@ Room
         })
     })
     .catch(err=> next(err))
-
-// router.post('/rooms',  (req, res, next) =>{
-//     Room
-//         .create(req.body)
-//         .then(room => {
-//             Room
-//                 .findAll()
-//                 .then(rooms => {
-//                     const json = JSON.stringify(rooms)
-//                     const stream = new Sse(rooms)
-//                     stream.updateInit(json)
-//                     stream.send(json)
-//                     res.status(201).send(room)
-//                 })
-//                 .catch(err => console.log(err))
-//         })
-//         .catch(err => next(err))
-// })
-
-// router.post('/rooms',  function (req, res, next) {
-//     Room
-//         .create(req.body)
-//         .then(room => res.json(room))
-//         .catch(err => next(err))
-// })
 
 router.get('/rooms/:id',  function (req, res, next) {
     const id = req.params.id
