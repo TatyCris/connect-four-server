c27ba24fc1e25592c65936ce002c05a062894873
“YvanSchaaik” <yvanschaaik@gmail.com>
2019-07-11 13:59:25 +0200

    ggpush

--- column/router.js
+++ column/router.js
@@ -17,29 +17,28 @@ router.get('/rooms/:id/columns', (req, res) => {
         .catch(err => next(err))
 })
 
-router.post('/rooms/:id/columns', (req, res, next) => {
-    Room
-        .findOne({ where: { id: req.params.id } })
-        .then(room => {
-            if (!room) {
-                res.status(404).send(room)
-            } else {
-                const roomId = req.params.id
-                for (i = 1; i < 8; i++) {
-                    Column.create({ index: i, roomId })
-                }
-                return res.status(201).send(room)
-            }
-        })
-        .catch(err => next(err))
-})
+// router.post('/room/:id/columns', (req, res, next) => {
+//     Room
+//         .findOne({ where: { id: req.params.id } })
+//         .then(room => {
+//             if (!room) {
+//                 res.status(404).send(room)
+//             } else {
+//                 const roomId = req.params.id
+//                 for (i = 1; i < 8; i++) {
+//                     Column.create({ index: i, roomId })
+//                 }
+//                 return res.status(201).send(room)
+//             }
+//         })
+//         .catch(err => next(err))
+// })
 
 router.put('/rooms/:id/columns', function (req, res, next) {
-    console.log('request test: ', req.body)
-    const { id } = req.params
-    const rows  = req.body
-    Column.findByPk(id)
-        .then(column => column.update( {rows:['1']}))
+    console.log('request test: ', req)
+    const roomId = req.params.id
+    const rows  = req.body.rows
+    Column.findByPk(roomId)
         .then(column => console.log('Column test: ',column)
         )
         .catch(err => next(err))
